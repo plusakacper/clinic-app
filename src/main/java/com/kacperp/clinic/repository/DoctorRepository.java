7762579b74a582bb22bdf8a22aae14163af4f0c8
plusakacper <plusakacper@gmail.com>
2024-09-17 17:14:56 +0200

    Added repositories

---
+++ src/main/java/com/kacperp/clinic/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.kacperp.clinic.repository;
+
+import com.kacperp.clinic.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
